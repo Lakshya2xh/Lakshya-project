0b64e9e656af97d9224c8d1bb17662af7e82c813
Lakshya2xh <bansallakshya968@gmail.com>
2024-09-16 16:32:22 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,22 @@
+operator = input("enter an operater(+,-,*,/):")
+a=float(input("enter the 1st number"))
+b=float(input("enter the 1nd number"))
+
+if operator=='+':
+    result=a+b
+    print(result)
+elif operator=='-':
+    result=a-b
+    print(result)
+elif operator=='*':
+    result=a*b
+    print(result)
+elif operater=='/':
+    if b!=0:
+        result=a/b
+        print(result)
+    else:
+        print("error:you can't devide something by zero")
+
+else:
+    print("invalid operator")
